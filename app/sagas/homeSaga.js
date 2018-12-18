d0f8573a00f21e79d7c8a04537b60beb31d33d71
baishu <baishu@maihaoche.com>
2018-12-18 19:24:59 +0800

    add husky

--- app/sagas/homeSaga.js
+++ app/sagas/homeSaga.js
@@ -1,77 +1,80 @@
-import {put, take, call, fork} from 'redux-saga/effects'
-import {get, post} from '../fetch/fetch'
-import {actionsTypes as IndexActionTypes} from '../reducers'
+/* eslint-disable camelcase */
+import {
+  put, take, call,
+} from 'redux-saga/effects';
+import { get, post } from '../fetch/fetch';
+import { actionsTypes as IndexActionTypes } from '../reducers';
 
+// eslint-disable-next-line
 export function* login(username, password) {
-    yield put({type: IndexActionTypes.FETCH_START});
-    try {
-        return yield call(post, '/user/login', {username, password})
-    } catch (error) {
-        yield put({type:IndexActionTypes.SET_MESSAGE,msgContent:'用户名或密码错误',msgType:0});
-    } finally {
-        yield put({type: IndexActionTypes.FETCH_END});
-    }
+  yield put({ type: IndexActionTypes.FETCH_START });
+  try {
+    return yield call(post, '/user/login', { username, password });
+  } catch (error) {
+    yield put({ type: IndexActionTypes.SET_MESSAGE, msgContent: '用户名或密码错误', msgType: 0 });
+  } finally {
+    yield put({ type: IndexActionTypes.FETCH_END });
+  }
 }
-
-export function* register (data) {
-    yield put({type:IndexActionTypes.FETCH_START});
-    try {
-        return yield call(post, '/user/register', data)
-    } catch (error) {
-        yield put({type:IndexActionTypes.SET_MESSAGE,msgContent:'注册失败',msgType:0});
-    } finally {
-        yield put({type: IndexActionTypes.FETCH_END});
-    }
+// eslint-disable-next-line
+export function* register(data) {
+  yield put({ type: IndexActionTypes.FETCH_START });
+  try {
+    return yield call(post, '/user/register', data);
+  } catch (error) {
+    yield put({ type: IndexActionTypes.SET_MESSAGE, msgContent: '注册失败', msgType: 0 });
+  } finally {
+    yield put({ type: IndexActionTypes.FETCH_END });
+  }
 }
 
 export function* loginFlow() {
-    while (true) {
-        let request = yield take(IndexActionTypes.USER_LOGIN);
-        let response = yield call(login, request.username, request.password);
-        if(response&&response.code === 0){
-            yield put({type:IndexActionTypes.SET_MESSAGE,msgContent:'登录成功!',msgType:1});
-            yield put({type:IndexActionTypes.RESPONSE_USER_INFO,data:response.data})
-        }
+  while (true) {
+    const request = yield take(IndexActionTypes.USER_LOGIN);
+    const response = yield call(login, request.username, request.password);
+    if (response && response.code === 0) {
+      yield put({ type: IndexActionTypes.SET_MESSAGE, msgContent: '登录成功!', msgType: 1 });
+      yield put({ type: IndexActionTypes.RESPONSE_USER_INFO, data: response.data });
     }
+  }
 }
 
-export function* registerFlow () {
-    while(true){
-        let request = yield take(IndexActionTypes.USER_REGISTER);
-        let response = yield call(register, request.data);
-        if(response&&response.code === 0){
-            yield put({type:IndexActionTypes.SET_MESSAGE,msgContent:'注册成功!',msgType:1});
-            yield put({type:IndexActionTypes.RESPONSE_USER_INFO,data:response.data})
-        }
-
+export function* registerFlow() {
+  while (true) {
+    const request = yield take(IndexActionTypes.USER_REGISTER);
+    const response = yield call(register, request.data);
+    if (response && response.code === 0) {
+      yield put({ type: IndexActionTypes.SET_MESSAGE, msgContent: '注册成功!', msgType: 1 });
+      yield put({ type: IndexActionTypes.RESPONSE_USER_INFO, data: response.data });
     }
+  }
 }
 
-export function* user_auth () {
-    while(true){
-        yield take(IndexActionTypes.USER_AUTH);
-        try {
-            yield put({type:IndexActionTypes.FETCH_START});
-            let response = yield call(get,'/user/userInfo');
-            if(response && response.code === 0){
-                yield put({type:IndexActionTypes.RESPONSE_USER_INFO,data:response.data})
-            }
-        }catch (err){
-            console.log(err);
-        }finally {
-            yield put({type: IndexActionTypes.FETCH_END});
-        }
+export function* user_auth() {
+  while (true) {
+    yield take(IndexActionTypes.USER_AUTH);
+    try {
+      yield put({ type: IndexActionTypes.FETCH_START });
+      const response = yield call(get, '/user/userInfo');
+      if (response && response.code === 0) {
+        yield put({ type: IndexActionTypes.RESPONSE_USER_INFO, data: response.data });
+      }
+    } catch (err) {
+      console.log(err);
+    } finally {
+      yield put({ type: IndexActionTypes.FETCH_END });
     }
+  }
 }
 
-export function* user_logout () {
-    while (true) {
-        let request = yield take(IndexActionTypes.USER_LOGOUT);
-        let response = yield call(get,'/user/logout');
-        console.log('request response', request, response)
-        if(response&&response.code === 0){
-            yield put({type:IndexActionTypes.SET_MESSAGE,msgContent:'成功退出登录',msgType:1});
-            yield put({type:IndexActionTypes.RESPONSE_USER_INFO,data:response.data})
-        }
+export function* user_logout() {
+  while (true) {
+    const request = yield take(IndexActionTypes.USER_LOGOUT);
+    const response = yield call(get, '/user/logout');
+    console.log('request response', request, response);
+    if (response && response.code === 0) {
+      yield put({ type: IndexActionTypes.SET_MESSAGE, msgContent: '成功退出登录', msgType: 1 });
+      yield put({ type: IndexActionTypes.RESPONSE_USER_INFO, data: response.data });
     }
+  }
 }
--- app/sagas/index.js
+++ app/sagas/index.js
@@ -1,27 +1,31 @@
-import {fork} from 'redux-saga/effects'
-import {loginFlow, registerFlow, user_auth, user_logout} from './homeSaga'
-import {get_all_users_flow} from './adminManagerUsersSaga'
-import {getAllTagsFlow, addTagFlow, delTagFlow} from './adminManagerTagsSaga'
-import {saveArticleFlow} from './adminManagerNewArticleSaga'
-import {getArticleListFlow,deleteArticleFlow,editArticleFlow} from './adminManagerArticleSaga'
-import {getArticlesListFlow,getArticleDetailFlow} from './frontSaga'
-import {creatLogs,addLogs} from './adminlogSaga'
+import { fork } from 'redux-saga/effects';
+import {
+  // eslint-disable-next-line camelcase
+  loginFlow, registerFlow, user_auth, user_logout,
+} from './homeSaga';
+// eslint-disable-next-line camelcase
+import { get_all_users_flow } from './adminManagerUsersSaga';
+import { getAllTagsFlow, addTagFlow, delTagFlow } from './adminManagerTagsSaga';
+import { saveArticleFlow } from './adminManagerNewArticleSaga';
+import { getArticleListFlow, deleteArticleFlow, editArticleFlow } from './adminManagerArticleSaga';
+import { getArticlesListFlow, getArticleDetailFlow } from './frontSaga';
+import { creatLogs, addLogs } from './adminlogSaga';
 
 export default function* rootSaga() {
-    yield  fork(loginFlow);
-    yield  fork(registerFlow);
-    yield  fork(user_auth);
-    yield  fork(user_logout);
-    yield fork(get_all_users_flow);
-    yield fork(getAllTagsFlow);
-    yield fork(addTagFlow);
-    yield fork(delTagFlow);
-    yield fork(saveArticleFlow);
-    yield fork(getArticleListFlow);
-    yield fork(deleteArticleFlow);
-    yield fork(getArticlesListFlow);
-    yield fork(getArticleDetailFlow);
-    yield fork(editArticleFlow);
-    yield fork(creatLogs);
-    yield fork(addLogs);
+  yield fork(loginFlow);
+  yield fork(registerFlow);
+  yield fork(user_auth);
+  yield fork(user_logout);
+  yield fork(get_all_users_flow);
+  yield fork(getAllTagsFlow);
+  yield fork(addTagFlow);
+  yield fork(delTagFlow);
+  yield fork(saveArticleFlow);
+  yield fork(getArticleListFlow);
+  yield fork(deleteArticleFlow);
+  yield fork(getArticlesListFlow);
+  yield fork(getArticleDetailFlow);
+  yield fork(editArticleFlow);
+  yield fork(creatLogs);
+  yield fork(addLogs);
 }
